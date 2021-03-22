bcf64e4b3baed0d4acf6a656dc5bd1c3daf351bc
Kunal Sailor <71784600+kunal25997@users.noreply.github.com>
2021-03-21 22:54:53 -0400

    Add files via upload
    Updated

---
+++ your_wellbeing_template/newsletter.php
@@ -0,0 +1,25 @@
+<?php
+    include 'header.php';
+    include 'footer.php';
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Welcome to Newsletter</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="css/news_style.css">
+    </head>
+    <body>
+    <div  class="form-box">
+        <h1>Subscribe</h1>
+        <div class="txt-e">
+            <label>Email</label>
+            <input type="text" name="email">
+            <span style="color: red;"></span>
+        </div>
+
+        <a id="btn" name="Subscribe">Subscribe</a>
+    </div>
+    </body>
+</html>
