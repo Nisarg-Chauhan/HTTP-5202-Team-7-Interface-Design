32e0815c8183268b7b43248b2df449b1b838d08e
Nodir Safarov <69470037+Nodrik777@users.noreply.github.com>
2021-04-12 21:06:50 -0400

    Add files via upload
    update

--- Models/users.php
+++ Models/users.php
@@ -21,16 +21,27 @@
             return $count;
         }
         
-        public function deleteUser($id, $db){
+       public function getUser($dbcon){
             
-            $sql = "DELETE FROM users WHERE id = :id";
+            $sql = "SELECT * FROM users where ";
+            $statement = $dbcon->prepare($sql);
+            $statement->execute();
             
-            $statement = $db->prepare($sql);
-            $statement->bindParam(':id', $id);
-            $count = $statement->execute();
-            return $count;
-            
+            $exercises = $statement->fetchAll(PDO::FETCH_OBJ);
+            return $exercises;
         }
+        
+        
+        /*public function getUser($email, $password, $db){
+        
+        $sql = "select *from login where email = '$email' and password = '$password'";  
+        $result = mysqli_query($db, $sql);  
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
+        $count = mysqli_num_rows($result);  
+        
+        
+        }*/
+        
         
         public function updateUser($id, $fname, $lname, $email, $phone, $password, $age, $db){
             
