590a8e621ac9f289b498bf68696073ccfc007aa6
Nodir Safarov <69470037+Nodrik777@users.noreply.github.com>
2021-04-16 12:39:08 -0400

    Add files via upload
    updating faq feature

--- faq.php
+++ faq.php
@@ -1,4 +1,17 @@
-<?php include 'header.php'; ?>
+<?php 
+
+
+    include 'header.php'; 
+    require_once 'Models/Database.php';
+    require_once 'Models/faq_db.php';
+    
+    $dbcon = Database::getDb();
+    $blog = new Faq();
+    $faqs =  $blog->getAllFaqs($dbcon);
+    
+?>
+
+?>
 
 <head>
 <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
@@ -9,56 +22,18 @@
 <h2>Frequently Asked Questions</h2>
 <section class="faq-block" id="faq-main" >
    <div class="container">
-      <ul class="faq">
-         <li class="faq-item ">
-            <a id="faq-link-1-1" href="#faq-1-1" role="tab" class="faq-title">Who do I contact for press inquiries?</a>
-            <div id="faq-1-1" class="faq-content">
-               <p>For press coverage and inquiries, please email: <a href="press@yourwellbeing.com">WellBeing </p>
-            </div>
-         </li>
-         <li class="faq-item ">
-            <a id="faq-link-1-2" href="#faq-1-2" role="tab" class="faq-title">How do I find a club near me and sign up for a membership?</a>
-            <div id="faq-1-2" class="faq-content">
-               <p>If you would like to join or find a specific <strong>Your Wellbeing</strong>, please use the <a href="/find-a-studio/">Find a Studio</a> page.</p>
-            </div>
-         </li>
-         <li class="faq-item ">
-            <a id="faq-link-1-3" href="#faq-1-3" role="tab" class="faq-title">Who do I contact if I have billing questions?</a>
-            <div id="faq-1-3" class="faq-content">
-               <p>Please contact the studio where you signed up for your membership with any billing questions or to change your billing information.</p>
-            </div>
-         </li>
-         <li class="faq-item ">
-            <a id="faq-link-1-4" href="#faq-1-4" role="tab" class="faq-title">Who do I contact with feedback (suggestions, great service, etc.)?</a>
-            <div id="faq-1-4" class="faq-content">
-               <p>If you’d like to contact a specific Studio head to their website to see their contact options, otherwise send us a message on our Facebook page to contact our support team. Click the Facebook Logo below to head to our page.</p>
-            </div>
-         </li>
-         <li class="faq-item ">
-            <a id="faq-link-1-5" href="#faq-1-5" role="tab" class="faq-title">How much does a membership cost?</a>
-            <div id="faq-1-5" class="faq-content">
-               <p>Each studio is individually owned and pricing changes slightly across locations. Please use the Find a Studio page to contact your nearest studio for pricing.</p>
-            </div>
-         </li>
-         <li class="faq-item ">
-            <a id="faq-link-1-6" href="#faq-1-6" role="tab" class="faq-title">Can I be a member of more than one studio?</a>
-            <div id="faq-1-6" class="faq-content">
-               <p>The membership covers 1 location. You will find that once you start training with a gym and become friends with people who train there and the staff you are not likely to change to another gym. If you would like to train at multiple locations most studios offer a 10 pass which you can pre-purchase. Please use the Find a Studio page to contact your nearest studio for pricing.</p>
-            </div>
-         </li>
-         <li class="faq-item ">
-            <a id="faq-link-1-7" href="#faq-1-7" role="tab" class="faq-title">What are the Your Wellbeing workouts ?</a>
-            <div id="faq-1-7" class="faq-content">
-               <p>Each class is a HIIT workout, no workout is the same, we offer 3 cardio and 3 resistance days with 1 super circuit that has both resistance and cardio . Please visit the site for the studio closest to you for further updates and class timetables.</p>
-            </div>
-         </li>
-         <li class="faq-item ">
-            <a id="faq-link-1-8" href="#faq-1-8" role="tab" class="faq-title">How do I book in for a class at my closest studio?</a>
-            <div id="faq-1-8" class="faq-content">
-               <p>Each studio is individually owned you will need to use the <a href="/find-a-studio/">Find a Studio</a> page to contact the studio to book in your classes and see the timetables.</p>
-            </div>
-         </li>
-      </ul>
+      <?php 
+        print '<dl class="mylist">';
+        foreach ($faqs as $item) {
+            
+            print '<dt><b>' . $item->question . '</b></dt></b>';
+            print '<dd>' . $item->answer . '</dd></b>';
+        }
+       
+       print '</dl>';
+       
+                      
+       ?>
    </div>
 </section>
 </main>
