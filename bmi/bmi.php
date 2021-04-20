c49741eada1dec8d213b8f60bc3579082ab94d33
Nodir Safarov <69470037+Nodrik777@users.noreply.github.com>
2021-04-19 21:19:36 -0400

    Add files via upload
    updating changes

--- bmi/bmi.php
+++ bmi/bmi.php
@@ -1,16 +1,15 @@
-<?php include '../header.php';
-    
+<?php 
+
+include 'header.php';
+
+require_once 'Models/Database.php';
+require_once 'Models/users.php';
+require_once 'Models/user_data.php';
+
     session_start();
 
-    $message = "";
+    $message = $bmi = "";
   
-
-/*function calculate() 
-{
-  
-  echo  $GLOBALS['message'] = round($GLOBALS['weight'] / pow(($GLOBALS['height']/'100'),2),2);        
-
-} */
 
 
 if(isset($_POST['submit']))
@@ -21,11 +20,38 @@ if(isset($_POST['submit']))
     
     //calculate();
     
-    $message = round($weight / pow(($height/100),2),2);
+    $bmi = round($weight / pow(($height/100),2),2);
+    
+    if($bmi < 18.5)
+        {
+         $message = "You are underweight.";
+        }
+        else if($bmi >=18.5 && $bmi <= 24.9)
+        {
+         $message = "Congrats!!! You have normal weight.";
+        }
+        else if($bmi >24.9 && $bmi <=29.9)
+        {
+         $message = "You are overweight.";
+        }
+        else
+        {
+         $message = "Be careful!!! You are obese.";
+        } 
     
     $_SESSION['your_weight'] = $weight;
     $_SESSION['your_height'] = $height;
-    $_SESSION['your_bmi'] = $message;
+    $_SESSION['your_bmi'] = $bmi;
+    
+    if(isset($_SESSION['login'])) {
+    $dbcon=Database::getDb();
+    $info=new UserData();
+	$c=$info->addUserData($height, $weight, $bmi, $dbcon);
+        
+    } else {
+        
+        echo 'error in adding info';
+    }
     
 } 
  
@@ -37,29 +63,30 @@ if(isset($_POST['submit']))
 <link rel="stylesheet" type="text/css" href="./css/bmi.css">
 
 <main class="container planner">
-    <h1>Body Mass Index Calculator</h1>
+    <h1>How to calculate Body Mass Index</h1>
+    
+    
+    <p>Body Mass Index is a simple calculation using a person’s height and weight. The formula is BMI = kg/m2 where kg is a person’s weight in kilograms and m2 is their height in metres squared.</p> 
+    <p>A BMI of 25.0 or more is overweight, while the healthy range is 18.5 to 24.9. BMI applies to most adults 18-65 years.</p>
+
+    <h3>Who shouldn't use a BMI calculator</h3>
+    <p>BMI is not used for muscle builders, long distance athletes, pregnant women, the elderly or young children. This is because BMI does not take into account whether the weight is carried as muscle or fat, just the number. Those with a higher muscle mass, such as athletes, may have a high BMI but not be at greater health risk. Those with a lower muscle mass, such as children who have not completed their growth or the elderly who may be losing some muscle mass may have a lower BMI. During pregnancy and lactation, a woman's body composition changes, so using BMI is not appropriate.</p>
+<h3 id="check">Check your BMI</h3>
     <div class="row justify-content-sm-center">
         <div class="col col-sm-8 col-lg-6">
             <form action="" method="post">
                 <div class="form-group">
-                    <label for="height">Enter your height:</label>
+                    <label for="height">Height in cm:</label>
                     <input type="number" class="form-control" name="height" id="height" placeholder="Enter your height">
-                    <select class="data" name="heightunits" id="heightunits">    
-                        <option class="option" value="metres" selected="selected">metres</option>
-                        <option class="option" value="inches">inches</option>
-                    </select>
                 </div>
                 <div class="form-group">
-                    <label for="weight">Enter your weight:</label>
+                    <label for="weight">Weight in kg:</label>
                     <input type="number" class="form-control"  name="weight" id="weight" placeholder="Enter your weight">
-                    <select class="data" name="weightunits" id="weightunits">
-                        <option value="kg" selected="selected">kilograms</option>
-                        <option value="lb">pounds</option>
-                    </select>
                 </div>
                 <button type="submit" name="submit" class="button">Calculate</button>
                 <div>
-                    <h4 class="bmi-result"> Your BMI is: <?php  echo $message; ?> </h4>
+                    <h4 class="bmi-result"> Your BMI is: <?php  echo $bmi; ?> </h4>
+                    <p class="bmi-result"> <?php  echo $message; ?> </9>
                 </div> 
                  <?php 
           
@@ -72,4 +99,4 @@ if(isset($_POST['submit']))
     </div>
 </main>
 
-<?php include '../footer.php'  ?>
+<?php include 'footer.php'  ?>
