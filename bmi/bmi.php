1f287a17a2911b9a15896351aeb39dc7a0195c4e
Nodir Safarov <69470037+Nodrik777@users.noreply.github.com>
2021-04-21 17:55:31 -0400

    Add files via upload

--- bmi/bmi.php
+++ bmi/bmi.php
@@ -4,7 +4,7 @@ include '../header.php';
 
 require_once '../Models/Database.php';
 require_once '../Models/users.php';
-require_once '../Models/user_data.php';
+
 
     session_start();
 
@@ -43,27 +43,20 @@ if(isset($_POST['submit']))
     $_SESSION['your_height'] = $height;
     $_SESSION['your_bmi'] = $bmi;
     
-    if(isset($_SESSION['login'])) {
-    $dbcon=Database::getDb();
-    $info=new UserData();
-	$c=$info->addUserData($height, $weight, $bmi, $dbcon);
-        
-    } else {
-        
-        echo 'error in adding info';
-    }
     
-} 
+}
+
  
 ?>
     
 
 
 
-<link rel="stylesheet" type="text/css" href="../css/bmi.css">
+<link rel="stylesheet" href="../css/bmi.css" type="text/css">
 
-<main class="container planner">
-    <h1>How to calculate Body Mass Index</h1>
+<main class="container">
+    <div class="text_area">
+    <h3>How to calculate Body Mass Index</h3>
     
     
     <p>Body Mass Index is a simple calculation using a person’s height and weight. The formula is BMI = kg/m2 where kg is a person’s weight in kilograms and m2 is their height in metres squared.</p> 
@@ -71,7 +64,8 @@ if(isset($_POST['submit']))
 
     <h3>Who shouldn't use a BMI calculator</h3>
     <p>BMI is not used for muscle builders, long distance athletes, pregnant women, the elderly or young children. This is because BMI does not take into account whether the weight is carried as muscle or fat, just the number. Those with a higher muscle mass, such as athletes, may have a high BMI but not be at greater health risk. Those with a lower muscle mass, such as children who have not completed their growth or the elderly who may be losing some muscle mass may have a lower BMI. During pregnancy and lactation, a woman's body composition changes, so using BMI is not appropriate.</p>
-<h3 id="check">Check your BMI</h3>
+    </div>
+        <h3 id="check">Check your BMI</h3>
     <div class="row justify-content-sm-center">
         <div class="col col-sm-8 col-lg-6">
             <form action="" method="post">
@@ -86,7 +80,7 @@ if(isset($_POST['submit']))
                 <button type="submit" name="submit" class="button">Calculate</button>
                 <div>
                     <h4 class="bmi-result"> Your BMI is: <?php  echo $bmi; ?> </h4>
-                    <p class="bmi-result"> <?php  echo $message; ?> </9>
+                    <p class="bmi-result"> <?php  echo $message; ?> </p>
                 </div> 
                  <?php 
           
