541227844b74a79b5b5f9ab67635987f714f6df6
Nodir Safarov <69470037+Nodrik777@users.noreply.github.com>
2021-04-22 12:50:24 -0400

    Update faq_db.php

--- Models/faq_db.php
+++ Models/faq_db.php
@@ -1,4 +1,4 @@
-<?php
+<<?php
     
     class Faq
     {
@@ -23,6 +23,49 @@
             
             $faq = $statement->fetchAll(PDO::FETCH_OBJ);
             return $faq;
+        }
+        
+        
+        public function addFaq($question, $answer, $db)
+        {
+            $sql = "INSERT INTO faq (question, answer) 
+            VALUES (:question, :answer )";
+            $statement = $db->prepare($sql);            
+            $statement->bindParam(':question', $question);
+            $statement->bindParam(':answer', $answer);        
+            
+            $count = $statement->execute();
+            return $count;
+        }
+        
+               
+        
+        public function updateFaq($id, $question, $answer, $db){
+            
+            $sql = "UPDATE faq
+            SET question = :question,
+                answer = :answer
+                WHERE id = :id";
+            
+            $statement =  $db->prepare($sql);
+            $statement->bindParam(':question', $question);
+            $statement->bindParam(':answer', $answer);
+            $statement->bindParam(':id', $id);
+            $count = $statement->execute();
+            
+            return $count;
+        }
+        
+                
+        public function deleteFaq($id, $db){
+            
+            $sql = "DELETE FROM faq WHERE id = :id";
+            
+            $statement = $db->prepare($sql);
+            $statement->bindParam(':id', $id);
+            $count = $statement->execute();
+            return $count;
+            
         }
         
          
