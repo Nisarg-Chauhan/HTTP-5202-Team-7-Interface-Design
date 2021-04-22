92c27511ed2fab48f790672aa3809b33ca3f01ce
Nodir Safarov <69470037+Nodrik777@users.noreply.github.com>
2021-04-22 06:56:27 -0400

    Update registration.php

--- login/registration.php
+++ login/registration.php
@@ -1,6 +1,6 @@
 <?php 
 
-include '../template/header.php';
+include 'header.php';
 
 require_once '../Models/Database.php';
 require_once '../Models/users.php';
@@ -136,4 +136,4 @@ if(isset($_POST['register'])){
         </div>
     </div>
 </main>
-<?php include '../template/footer.php'; ?>
+<?php include 'footer.php'; ?>
