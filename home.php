f435bf582408bb26a7fcf954bebcb295e08cef76
Kunal Sailor <71784600+kunal25997@users.noreply.github.com>
2021-04-23 00:50:08 -0400

    Main homepage file

---
+++ home.php
@@ -0,0 +1,70 @@
+<?php
+    include_once "header.php";
+?>
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/spikeball-rAUEs1wKfhI-unsplash.jpg" class="d-block w-100" alt="exercise 1">
+      <!-- Photo by <a href="https://unsplash.com/@spikeball?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Spikeball</a> on <a href="https://unsplash.com/s/photos/exercise?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a> -->
+    </div>
+    <div class="carousel-item">
+      <img src="images/boxed-water-is-better-6aEjZbs53K0-unsplash.jpg" class="d-block w-100" alt="exercise 2">
+      <!-- Photo by <a href="https://unsplash.com/@boxedwater?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Boxed Water Is Better</a> on <a href="https://unsplash.com/s/photos/yoga?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a> -->
+    </div>
+    <div class="carousel-item">
+      <img src="images/jannis-brandt-mmsQUgMLqUo-unsplash.jpg" class="d-block w-100" alt="exercise 3">
+      <!-- Photo by <a href="https://unsplash.com/@jannisbrandt?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Jannis Brandt</a> on <a href="https://unsplash.com/s/photos/fitness?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a> -->
+    </div>
+    <div class="carousel-item">
+        <img src="images/anastase-maragos-FP7cfYPPUKM-unsplash.jpg" class="d-block w-100" alt="exercise 4">
+        <!-- Photo by <a href="https://unsplash.com/@visualsbyroyalz?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Anastase Maragos</a> on <a href="https://unsplash.com/s/photos/workout?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a> -->
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-sm-8">
+      <h2>About Your Wellbeing</h2>     
+      <p>Your Wellbeing is a fitness planner website that allows it's users to get their exercise planner according 
+      to their BMI(Body Mass Index). We also sell sponsored products of different brands. We also have different kinds of 
+      diet charts which can be followed to keep yourself stay healthy and fit. All of our products and planners are made 
+      considering the latest trends in the world.</p>
+      <button class="btn btn-default btn-lg"><a href="#">Get in Touch</a></button>
+    </div>
+    <div class="col-sm-4">
+      <span class="glyphicon glyphicon-signal logo"></span>
+    </div>
+  </div>
+</div>
+
+<div class="container-fluid bg-grey">
+  <div class="row">
+    <div class="col-sm-4">
+      <span class="glyphicon glyphicon-globe logo"></span>
+    </div>
+    <div class="col-sm-8">
+      <h2>Our Values</h2> <br />
+      <h4><strong>MISSION:</strong> <em>Our mission is to provide functional and innovative experience of uncomprising quality.</em></h4> <br />      
+      <h4><strong>VISION:</strong> <em>Health and wellness describes your ideal state of physical and emotional health.</em></h4>
+    </div>
+  </div>
+</div>
+
+<?php
+    include_once "footer.php";
+?>
