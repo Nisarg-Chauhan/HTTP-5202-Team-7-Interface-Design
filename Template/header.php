2b04d153df87ead1c87fdcdb8c7935711e90d137
Nisarg-Chauhan <46188042+Nisarg-Chauhan@users.noreply.github.com>
2021-04-25 16:16:46 -0400

    added kunal's db

--- Template/header.php
+++ Template/header.php
@@ -9,8 +9,8 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <link rel="stylesheet" type="text/css" href="css/about.css">
-    <link rel="stylesheet" type="text/css" href="css/content.css">
+    <link rel="stylesheet" type="text/css" href="../css/about.css">
+    <link rel="stylesheet" type="text/css" href="../css/content.css">
     <link rel="stylesheet" type="text/css" href="../css/planner.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
@@ -24,7 +24,7 @@
 
         <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
             <div class="navbar-nav">
-                <a href="home.php" class="nav-item nav-link active home-lnk">Home</a>
+                <a href="../home/home.php" class="nav-item nav-link active home-lnk">Home</a>
                 <a href="#" class="nav-item nav-link about-lnk">About</a>
                 <div class="nav-item dropdown">
                     <a href="#" class="nav-link dropdown-toggle planner-lnk" data-toggle="dropdown">Planner</a>
--- login/login.php
+++ login/login.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 include '../template/header.php';
 
@@ -6,36 +6,36 @@ include '../template/header.php';
 require_once '../Models/Database.php';
 require_once '../Models/users.php';
 //require_once '../Models/Testimonials.php';
-  
-if($_SERVER["REQUEST_METHOD"] == "POST") {
-      // username and password sent from form 
-      
-      $username = $_POST['email'];  
-      $password = md5($_POST['pwd']); 
-      $dbcon=Database::getDb();
-      $message_err = "";
-      
-       $_SESSION['login'] = $_POST['email']; 
-    
-      $newUser=new User();     
-     $user=$newUser->getCurrentUser($username,$password, $dbcon);
-            
-      
-      if(isset($user) && $user!="") {
-          
-        session_start();  
-         $_SESSION['login'] = $_POST['email']; 
-         $_SESSION['userId']=$user->id;
-          $_SESSION['role'] = $user->role; 
-         
-         
-         header("location:../home.php");
-      }else {
-         $message_err = "Username or Password not valid";
-      }
-   }
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // username and password sent from form 
+
+    $username = $_POST['email'];
+    $password = md5($_POST['pwd']);
+    $dbcon = Database::getDb();
+    $message_err = "";
+
+    $_SESSION['login'] = $_POST['email'];
+
+    $newUser = new User();
+    $user = $newUser->getCurrentUser($username, $password, $dbcon);
+
+
+    if (isset($user) && $user != "") {
+
+        session_start();
+        $_SESSION['login'] = $_POST['email'];
+        $_SESSION['userId'] = $user->id;
+        $_SESSION['role'] = $user->role;
+
+
+        header("location:../home/home.php");
+    } else {
+        $message_err = "Username or Password not valid";
+    }
+}
 ?>
-    
+
 
 
 <div style="height:650px;" class="container planner">
@@ -46,18 +46,18 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
                 <div class="form-group">
                     <label for="email">Email:</label>
                     <input type="email" class="form-control" name="email" id="email" placeholder="Enter email">
-                    <span style="color:red; font-zise: 8px; background:#f2f2f2;"><?= isset($login_err)? $login_err: ''; ?> </span>
+                    <span style="color:red; font-zise: 8px; background:#f2f2f2;"><?= isset($login_err) ? $login_err : ''; ?> </span>
                 </div>
                 <div class="form-group">
                     <label for="pwd">Password:</label>
-                    <input type="password"  name="pwd" class="form-control" id="pwd" placeholder="Enter password">
-                    <span style="color:red; font-zise: 8px; background:#f2f2f2;"><?= isset($pass_err)? $pass_err: ''; ?> </span>
+                    <input type="password" name="pwd" class="form-control" id="pwd" placeholder="Enter password">
+                    <span style="color:red; font-zise: 8px; background:#f2f2f2;"><?= isset($pass_err) ? $pass_err : ''; ?> </span>
                 </div>
                 <div>
-                    <span style="color:red; font-zise: 8px; background:#f2f2f2;"><?= isset($message_err)? $message_err: ''; ?> </span>
+                    <span style="color:red; font-zise: 8px; background:#f2f2f2;"><?= isset($message_err) ? $message_err : ''; ?> </span>
                 </div>
                 <button type="submit" name="login" class="button">Submit</button>
-                <button type="submit" class="button"><a href="registration.php" >Registration</a></button>
+                <button type="submit" class="button"><a href="registration.php">Registration</a></button>
             </form>
         </div>
     </div>
